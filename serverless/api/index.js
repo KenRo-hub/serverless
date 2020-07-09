cf816b0deb099dbab3757b328ab261a641bd9c90
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-09 11:36:51 -0500

    commit

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const Users = mongoose.model('User', new mongoose.Schema({name : String}, { useUnifiedTopology: true }, { useNewUrlParser: true })
+)
+
+Users.create({ name : 'Chanchito feliz'})
+
 app.get('*', (req, res) => {
-    console.log("Hola mundo! Primera aplicación serverless");
-    res.send({mensaje : "ya corrio :,v xd"});
+   Users.find()
+   .then(x => res.send(x))
 })
 
 module.exports = app;
