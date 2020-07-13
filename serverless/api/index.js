c079e908002ee94c1c1d3bc4f5136aee5c03b0ed
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-13 14:23:43 -0500

    commit

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const meals = require ('./routes/Meals')
 const orders = require ('./routes/Orders')
+
 const app = express();
 app.use(bodyParser.json())
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false  } );
 
