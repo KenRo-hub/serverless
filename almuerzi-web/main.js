cc512224d2eb78d794d5b4ba2010f754c0233c36
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-15 13:23:52 -0500

    commit

---
+++ almuerzi-web/main.js
@@ -0,0 +1,33 @@
+const stringToHtml = (s) =>{
+    const parser= new DOMParser()
+    const doc = parser.parseFromString(s,'text/html')
+    return doc.body.firstChild
+}
+
+const renderItem = (item) =>{
+    const element = stringToHtml(`<li data-id="${item._id}">${item.name}</li>`)
+
+    element.addEventListener('click', () =>{
+        const mealsList = document.getElementById('meals-list');
+        Array.from(mealsList.children).forEach(x => x.classList.remove('selected'))
+        element.classList.add('selected')
+        const mealsIdInput = document.getElementById('meals-id')
+        mealsIdInput.value = item._id
+    })
+
+    return element
+}
+
+window.onload = () => {
+fetch('https://serverless-weld-pi.vercel.app/api/meals')
+.then(response => response.json())
+.then(data => {
+    const mealsList = document.getElementById('meals-list');
+    const submit = document.getElementById('submit');
+    const listItems = data.map(renderItem)
+    mealsList.removeChild(mealsList.firstElementChild)
+    listItems.forEach(element => { mealsList.appendChild(element)
+    });
+    submit.removeAttribute('disabled');
+})
+}
